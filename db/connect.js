31a6b756e56017655ec6cb0a48bdbccb1d4896ab
Emma Zullo <zulloemma0@gmail.com>
2026-04-20 02:28:03 +0000

    new commit

--- db/connect.js
+++ db/connect.js
@@ -1,9 +1,7 @@
 const mongoose = require("mongoose");
 
-const DEFAULT_MONGODB_URI = "mongodb://127.0.0.1:27017/photo-sharing";
-
 async function connectToDatabase() {
-  const mongoUri = process.env.MONGODB_URI || DEFAULT_MONGODB_URI;
+  const mongoUri = process.env.MONGODB_URI;
 
   if (mongoose.connection.readyState === 1) {
     return mongoose.connection;
@@ -18,5 +16,4 @@ async function connectToDatabase() {
 
 module.exports = {
   connectToDatabase,
-  DEFAULT_MONGODB_URI,
 };
